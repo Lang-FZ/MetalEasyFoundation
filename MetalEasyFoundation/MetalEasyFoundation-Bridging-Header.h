7f8da162f227c60fd6fc7025fb5bd1dffb3715c3
Lang-FZ <446003664@qq.com>
2019-12-27 13:23:51 +0800

    fix

---
+++ MetalEasyFoundation/MetalEasyFoundation-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef SwiftExample_Objc_Bridge_Header_h
+#define SwiftExample_Objc_Bridge_Header_h
+
+#import "JGProgressHUD.h"
+//#import <JGProgressHUD/JGProgressHUD.h>
+
+#endif
